f5313fca5bb274a48751a65a7d7f223e05d313f2
Orest-Programmer <85691769+Orest-Programmer@users.noreply.github.com>
2022-10-22 12:00:36 +0300

    Add files via upload
    Add hm 10

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import ua.lviv.lgs.domain.User;
+import ua.lviv.lgs.service.UserService;
+import ua.lviv.lgs.service.impl.UserServiceImpl;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		UserService userService = new UserServiceImpl();
+//		userService.create(new User("test@test", "test", "test", "test", "test"));
+
+	}
+}
